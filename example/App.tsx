8a0500625b9c6790ddd4f0a0c2247586c0d23c71
FreakyCoder <kurayogun@gmail.com>
2022-12-03 15:33:11 +0300

    fix: main path

--- example/App.tsx
+++ example/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View} from 'react-native';
-import BasicModal, {Button} from './lib';
+import BasicModal, {Button} from 'react-native-basic-modal';
 
 interface AppProps {}
 
